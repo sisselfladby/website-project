5ea003f8f0b08368bfc49a05e7ebcb3b10d91153
Sissel Fladby <sisfla@gmail.com>
2019-11-14 20:02:16 +0100

    Rewrite App class to const

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Homepage from './pages/Home';
 import BusinessCase from './pages/BusinessCase';
 
-class App extends Component {
-  render() {
-    return (
-      <div id="app">
-        <Router>
-            <Switch>
-                <Route path='/business' component={() => <BusinessCase/>}/>
-                <Route path='/' component={() => <Homepage/>}/>
-            </Switch>
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div id="app">
+      <Router>
+        <Switch>
+            <Route path='/business' component={() => <BusinessCase/>}/>
+            <Route path='/' component={() => <Homepage/>}/>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
